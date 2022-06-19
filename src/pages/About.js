77813e559683475e51affb61f39baf1b5923638c
franky <frankydominique@users.noreply.github.com>
2022-06-18 23:32:07 -0400

    submitting

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/IMG_8188.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
         {/* <p>Design your About me page </p> */}
-        <div class="split left">
+        <div className="split left">
           <div className="centered">
             <img
               className="profile_image"
@@ -18,10 +18,24 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Franceska Padilla Coo</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              <div >
+              <h3> (she/her) 2024 Computer Science (B.S.), Data Science Minor at the University of Virginia </h3>
+              <hr />
+
+              <p> Hello! My name is Franceska, but you can also call me Franky! I'm currently interning as a data scientist 
+                at IT Concepts. I love listening to music, playing flag football, and dancing with my university's Filipino
+                organization!
+              </p>
+              </div>
+
+              <a href="https://www.linkedin.com/in/franceska-padilla-coo" target="_blank">
+                <img src={'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'} style={{width:'10%'}}/>
+              </a>
+              <a className="m-3" href="https://open.spotify.com/user/frankydominique" target="_blank">
+                <img src={'https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg'} style={{width:'10%'}}/>
+              </a>
             </div>
           </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,14 @@ import "../pages/Home.css";
 
 class Home extends Component {
   // Create a default state of this component with an empty list of todos.
-  constructor() {
+  constructor() { 
     super();
     this.state = {
       todos: [],
     };
+    this.deleteTodo = this.deleteTodo.bind(this)
   }
+
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -21,12 +23,31 @@ class Home extends Component {
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
+    let new_list = [this.state.todos]
+
+    if (this.state.todos.find(element => element.content === todo.content)) {
+      return;
+    } else {
+      new_list = [...this.state.todos, todo];
+    }
+
+    //let new_list = [...this.state.todos, todo];
+    
     // Update the local state with the new array.
     this.setState({
       todos: new_list,
     });
   };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  }
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +57,7 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
